8c0677e01187b2bf0b91768268c08b070ded998e
devil <devil.@devils-Mac-Studio.local>
2024-06-02 13:54:53 -0700

    finished exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,6 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    return string.repeat(num);
 };
-
+console.log(repeatString)
 // Do not edit below this line
 module.exports = repeatString;
