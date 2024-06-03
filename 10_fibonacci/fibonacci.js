007779d82f63c9f09e9669bc0339800ead20b362
devil <devil.@devils-Mac-Studio.local>
2024-06-02 23:27:06 -0700

    finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+  let intIndex = +index;
+  if (intIndex < 0) return "OOPS";
+  if (intIndex === 0) return 0;
+  if (intIndex === 1) return 1;
+  let a = 0,
+    b = 1;
+  for (let i = 2; i <= intIndex; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 };
 
 // Do not edit below this line
