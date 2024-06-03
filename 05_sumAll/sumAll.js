1f9c5dd7cf7c9e538ef49cb9758369116d2464bf
devil <devil.@devils-Mac-Studio.local>
2024-06-02 18:30:24 -0700

    simplified sumAll.js for easier readablity

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,18 +1,14 @@
-const sumAll = function(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        return 'ERROR'
-    }
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0)
+    return "ERROR";
 
-    if (a < 0 || b < 0) {
-        return 'ERROR'
-    }
-    const start = Math.min(a, b);
-    const end = Math.max(a, b);
-    let total = 0;
-    for (let i = start; i <= end; i++) {
-        total += i;
-    }
-    return total;
+  const start = Math.min(a, b);
+  const end = Math.max(a, b);
+  let total = 0;
+  for (let i = start; i <= end; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
