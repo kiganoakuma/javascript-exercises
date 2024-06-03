18a8d1eb2a732c9d8cb5fb88f9d3fb8bdc012dfd
devil <devil.@devils-Mac-Studio.local>
2024-06-02 22:50:14 -0700

    finished exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const filtered = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    const reverse = filtered.split('').reverse().join('');
+    return filtered === reverse;
 };
 
 // Do not edit below this line
