35d1d1c6275c29ed4311f42e5fa5543b8c418a46
devil <devil.@devils-Mac-Studio.local>
2024-06-02 18:23:55 -0700

    finished exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR'
+    }
 
+    if (a < 0 || b < 0) {
+        return 'ERROR'
+    }
+    const start = Math.min(a, b);
+    const end = Math.max(a, b);
+    let total = 0;
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
